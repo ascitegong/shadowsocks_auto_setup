ba1c328246fbd74cdeafe965fb2e572b1e0078c2
root <root@ascitegong.f3322.net>
2015-10-16 03:02:21 -0400

    Add ubuntu auto install script.

---
+++ ubuntu/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+apt-get update
+apt-get -y install python-setuptools python-m2crypto supervisor python-pip
+pip install shadowsocks
+
+which supervisord
+which ssserver
+
+cp shadowsocks_1.conf /etc/
+cp shadowsocks.conf /etc/supervisor/conf.d/
+
+service supervisor start
+
+sed -i 's|^\s*exit|#exit|' /etc/rc.local
+echo "" >> /etc/rc.local
+echo "service supervisor start;" >> /etc/rc.local
+echo "exit 0;" >> /etc/rc.local
+
+
