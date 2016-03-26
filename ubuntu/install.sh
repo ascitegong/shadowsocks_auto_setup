91a8e2f4de1e0f730883a21547f3236cc4d5c855
root <root@vultr.guest>
2016-03-26 12:49:28 +0000

    Enhance kernel params for kernel version >3.5, kvm only

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -10,11 +10,15 @@ which ssserver
 cp shadowsocks_1.json /etc/
 cp shadowsocks.conf /etc/supervisor/conf.d/
 
+cat limits.conf >> /etc/security/limits.conf
+cat sysctl.conf >> /etc/sysctl.conf
+
+ulimit -n 51200
 service supervisor start
 
 sed -i 's|^\s*exit|#exit|' /etc/rc.local
 echo "" >> /etc/rc.local
+echo "ulimit -n 51200;" >> /etc/rc.local
 echo "service supervisor start;" >> /etc/rc.local
 echo "exit 0;" >> /etc/rc.local
-
 
